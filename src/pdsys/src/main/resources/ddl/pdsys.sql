b8745fe033daa703096d9cb83006aa93241b082a
zhangxiaofengjs <zhangxiaofengjs@163.com>
2018-04-02 11:24:47 +0900

    更新ddl

--- src/pdsys/src/main/resources/ddl/pdsys.sql
+++ src/pdsys/src/main/resources/ddl/pdsys.sql
@@ -36,27 +36,47 @@ INSERT INTO `bom_tbl` (`c_id`, `c_pn`, `c_name`, `c_unit_id`, `c_supplier_id`, `
 	(4, 'X0S1', '包装箱', 4, 1, 1);
 /*!40000 ALTER TABLE `bom_tbl` ENABLE KEYS */;
 
+--  テーブル pdsys.customer_tbl の構造をダンプしています
+CREATE TABLE IF NOT EXISTS `customer_tbl` (
+  `c_id` int(11) DEFAULT NULL COMMENT 'ID',
+  `c_name` int(11) DEFAULT NULL COMMENT '顾客名',
+  `c_address` int(11) DEFAULT NULL COMMENT '地址',
+  `c_phone` int(11) DEFAULT NULL COMMENT '联系方式'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='顾客表 ';
+
+-- テーブル pdsys.customer_tbl: ~0 rows (約) のデータをダンプしています
+DELETE FROM `customer_tbl`;
+/*!40000 ALTER TABLE `customer_tbl` DISABLE KEYS */;
+/*!40000 ALTER TABLE `customer_tbl` ENABLE KEYS */;
+
 --  テーブル pdsys.order_item_tbl の構造をダンプしています
 CREATE TABLE IF NOT EXISTS `order_item_tbl` (
-  `c_id` int(11) DEFAULT NULL COMMENT 'ID',
+  `c_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `c_order_id` int(11) DEFAULT NULL COMMENT '订单号',
   `c_pn_id` int(11) DEFAULT NULL COMMENT '品番ID',
+  `c_pn_cls_id` int(11) DEFAULT NULL COMMENT '子类名ID',
   `c_num` float DEFAULT NULL COMMENT '数量',
-  `c_reject_ratio` float DEFAULT NULL COMMENT '不良率 0-1'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='订单条目表';
+  `c_reject_ratio` float DEFAULT '0' COMMENT '不良率 0-1',
+  PRIMARY KEY (`c_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='订单条目表';
 
--- テーブル pdsys.order_item_tbl: ~0 rows (約) のデータをダンプしています
+-- テーブル pdsys.order_item_tbl: ~4 rows (約) のデータをダンプしています
 DELETE FROM `order_item_tbl`;
 /*!40000 ALTER TABLE `order_item_tbl` DISABLE KEYS */;
+INSERT INTO `order_item_tbl` (`c_id`, `c_order_id`, `c_pn_id`, `c_pn_cls_id`, `c_num`, `c_reject_ratio`) VALUES
+	(1, 1, 1, 1, 2000.8, 0.2),
+	(2, 2, 1, 2, 233001, 0),
+	(3, 2, 2, 3, 2033300, 0),
+	(4, 2, 2, 4, 2000.83, 0);
 /*!40000 ALTER TABLE `order_item_tbl` ENABLE KEYS */;
 
 --  テーブル pdsys.order_tbl の構造をダンプしています
 CREATE TABLE IF NOT EXISTS `order_tbl` (
   `c_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `c_no` varchar(50) NOT NULL COMMENT '订单编号',
-  `c_order_date` datetime DEFAULT NULL COMMENT '下单时间',
-  `c_ship_dead_date` datetime DEFAULT NULL COMMENT '交货期限',
-  `c_ship_date` datetime DEFAULT NULL COMMENT '交货时间',
+  `c_order_date` date DEFAULT NULL COMMENT '下单时间',
+  `c_ship_dead_date` date DEFAULT NULL COMMENT '交货期限',
+  `c_ship_date` date DEFAULT NULL COMMENT '交货时间',
   `c_state` int(11) DEFAULT NULL COMMENT '状态 0：计划中 1：生产中 2：已完成 3：已删除',
   `c_comment` varchar(100) DEFAULT NULL COMMENT '备注',
   PRIMARY KEY (`c_id`),
@@ -67,14 +87,68 @@ CREATE TABLE IF NOT EXISTS `order_tbl` (
 DELETE FROM `order_tbl`;
 /*!40000 ALTER TABLE `order_tbl` DISABLE KEYS */;
 INSERT INTO `order_tbl` (`c_id`, `c_no`, `c_order_date`, `c_ship_dead_date`, `c_ship_date`, `c_state`, `c_comment`) VALUES
-	(1, 'JK-900-200', '2018-03-28 15:54:39', '2018-03-30 15:54:45', '2018-03-28 15:54:56', 0, NULL),
-	(2, 'JK-900-201', '2018-03-28 15:54:39', '2018-03-30 15:54:45', '2018-03-28 15:54:56', 1, NULL),
-	(3, 'JK-900-202', '2018-03-28 15:54:39', '2018-03-30 15:54:45', '2018-03-28 15:54:56', 0, NULL),
-	(4, 'JK-900-203', '2018-03-28 15:54:39', '2018-03-30 15:54:45', '2018-03-28 15:54:56', 1, NULL),
-	(5, 'JK-900-204', '2018-03-28 15:54:39', '2018-03-30 15:54:45', '2018-03-28 15:54:56', 2, NULL),
-	(6, 'JK-900-205', '2018-03-28 15:54:39', '2018-03-30 15:54:45', '2018-03-28 15:54:56', 2, NULL),
-	(7, 'JK-900-206', '2018-03-28 15:54:39', '2018-03-30 15:54:45', '2018-03-28 15:54:56', 3, NULL);
+	(1, 'JK-900-200', '2018-03-28', '2018-03-30', '2018-03-28', 0, NULL),
+	(2, 'JK-900-201', '2018-03-28', '2018-03-30', '2018-03-28', 1, NULL),
+	(3, 'JK-900-202', '2018-03-28', '2018-03-30', '2018-03-28', 0, NULL),
+	(4, 'JK-900-203', '2018-03-28', '2018-03-30', '2018-03-28', 1, NULL),
+	(5, 'JK-900-204', '2018-03-28', '2018-03-30', '2018-03-28', 2, NULL),
+	(6, 'JK-900-205', '2018-03-28', '2018-03-30', '2018-03-28', 2, NULL),
+	(7, 'JK-900-206', '2018-03-28', '2018-03-30', '2018-03-28', 3, NULL);
 /*!40000 ALTER TABLE `order_tbl` ENABLE KEYS */;
+
+--  テーブル pdsys.pn_cls_relation_tbl の構造をダンプしています
+CREATE TABLE IF NOT EXISTS `pn_cls_relation_tbl` (
+  `c_pn_id` int(11) NOT NULL,
+  `c_class_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='品目和子类对应表';
+
+-- テーブル pdsys.pn_cls_relation_tbl: ~6 rows (約) のデータをダンプしています
+DELETE FROM `pn_cls_relation_tbl`;
+/*!40000 ALTER TABLE `pn_cls_relation_tbl` DISABLE KEYS */;
+INSERT INTO `pn_cls_relation_tbl` (`c_pn_id`, `c_class_id`) VALUES
+	(1, 1),
+	(1, 2),
+	(2, 3),
+	(2, 4),
+	(3, 4),
+	(3, 3);
+/*!40000 ALTER TABLE `pn_cls_relation_tbl` ENABLE KEYS */;
+
+--  テーブル pdsys.pn_cls_tbl の構造をダンプしています
+CREATE TABLE IF NOT EXISTS `pn_cls_tbl` (
+  `c_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `c_name` varchar(50) NOT NULL COMMENT '名称',
+  PRIMARY KEY (`c_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='品番子分类';
+
+-- テーブル pdsys.pn_cls_tbl: ~4 rows (約) のデータをダンプしています
+DELETE FROM `pn_cls_tbl`;
+/*!40000 ALTER TABLE `pn_cls_tbl` DISABLE KEYS */;
+INSERT INTO `pn_cls_tbl` (`c_id`, `c_name`) VALUES
+	(1, '红色'),
+	(2, '蓝色'),
+	(3, '大号'),
+	(4, '小号');
+/*!40000 ALTER TABLE `pn_cls_tbl` ENABLE KEYS */;
+
+--  テーブル pdsys.pn_tbl の構造をダンプしています
+CREATE TABLE IF NOT EXISTS `pn_tbl` (
+  `c_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `c_pn` varchar(50) NOT NULL COMMENT '品番',
+  `c_name` varchar(50) NOT NULL COMMENT '名称',
+  `c_unit_id` int(11) NOT NULL COMMENT '单位ID',
+  PRIMARY KEY (`c_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='品番定义表';
+
+-- テーブル pdsys.pn_tbl: ~4 rows (約) のデータをダンプしています
+DELETE FROM `pn_tbl`;
+/*!40000 ALTER TABLE `pn_tbl` DISABLE KEYS */;
+INSERT INTO `pn_tbl` (`c_id`, `c_pn`, `c_name`, `c_unit_id`) VALUES
+	(1, 'X01', '纸杯', 1),
+	(2, 'X0555', '蛋糕托盘', 2),
+	(3, 'X03', '面包盒', 3),
+	(4, 'X031', '硅胶烤盘', 1);
+/*!40000 ALTER TABLE `pn_tbl` ENABLE KEYS */;
 
 --  テーブル pdsys.supplier_tbl の構造をダンプしています
 CREATE TABLE IF NOT EXISTS `supplier_tbl` (
@@ -110,6 +184,20 @@ INSERT INTO `unit_tbl` (`c_id`, `c_name`, `c_sub_unit_id`, `c_ratio`) VALUES
 	(4, '个', -1, 0);
 /*!40000 ALTER TABLE `unit_tbl` ENABLE KEYS */;
 
+--  テーブル pdsys.user_tbl の構造をダンプしています
+CREATE TABLE IF NOT EXISTS `user_tbl` (
+  `c_id` int(11) DEFAULT NULL COMMENT 'ID',
+  `c_no` varchar(50) DEFAULT NULL COMMENT '工番',
+  `c_name` varchar(50) DEFAULT NULL COMMENT '名字',
+  `c_phone` varchar(50) DEFAULT NULL COMMENT '手机',
+  `c_address` varchar(50) DEFAULT NULL COMMENT '地址'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户定义表';
+
+-- テーブル pdsys.user_tbl: ~0 rows (約) のデータをダンプしています
+DELETE FROM `user_tbl`;
+/*!40000 ALTER TABLE `user_tbl` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_tbl` ENABLE KEYS */;
+
 --  テーブル pdsys.warehouse_bom_tbl の構造をダンプしています
 CREATE TABLE IF NOT EXISTS `warehouse_bom_tbl` (
   `c_id` int(11) DEFAULT '0' COMMENT 'ID',
@@ -127,18 +215,23 @@ INSERT INTO `warehouse_bom_tbl` (`c_id`, `c_bom_id`, `c_num`) VALUES
 	(4, 4, 98);
 /*!40000 ALTER TABLE `warehouse_bom_tbl` ENABLE KEYS */;
 
---  テーブル pdsys.warehouse_order_item_tbl の構造をダンプしています
-CREATE TABLE IF NOT EXISTS `warehouse_order_item_tbl` (
+--  テーブル pdsys.warehouse_pn_tbl の構造をダンプしています
+CREATE TABLE IF NOT EXISTS `warehouse_pn_tbl` (
   `c_id` int(11) DEFAULT NULL COMMENT 'ID',
   `c_order_item_id` int(11) DEFAULT NULL COMMENT '订单条目ID',
   `c_type` int(11) DEFAULT NULL COMMENT '半成品(0) 成品(1)',
   `c_num` float DEFAULT NULL COMMENT '数量'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='订单条目仓库表 		\r\n';
 
--- テーブル pdsys.warehouse_order_item_tbl: ~0 rows (約) のデータをダンプしています
-DELETE FROM `warehouse_order_item_tbl`;
-/*!40000 ALTER TABLE `warehouse_order_item_tbl` DISABLE KEYS */;
-/*!40000 ALTER TABLE `warehouse_order_item_tbl` ENABLE KEYS */;
+-- テーブル pdsys.warehouse_pn_tbl: ~4 rows (約) のデータをダンプしています
+DELETE FROM `warehouse_pn_tbl`;
+/*!40000 ALTER TABLE `warehouse_pn_tbl` DISABLE KEYS */;
+INSERT INTO `warehouse_pn_tbl` (`c_id`, `c_order_item_id`, `c_type`, `c_num`) VALUES
+	(1, 1, 1, 1256),
+	(2, 2, 0, 698),
+	(3, 3, 0, 698),
+	(4, 4, 0, 698);
+/*!40000 ALTER TABLE `warehouse_pn_tbl` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
