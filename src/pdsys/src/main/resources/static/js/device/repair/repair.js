81b6c253daa6df3e87cd1031974033f6f3d78195
zhaiq <zhaiqiang@hyron.com>
2018-05-28 17:16:21 +0900

    合计

--- src/pdsys/src/main/resources/static/js/device/repair/repair.js
+++ src/pdsys/src/main/resources/static/js/device/repair/repair.js
@@ -1,75 +1,76 @@
 $(document).ready(function(){
 	
-	//排序
-	function CellSort(tableId)
-	{
-		var table =document.getElementById(tableId);
-		var rows = table.rows.length;
-		//除去表头和列名所在行
-		for (i=1;i<rows-1;i++)
-		{
-			k=i;
-			for (j=i+1;j<rows;j++)
-			{
-			 
-				if (table.rows[k].cells[0].innerHTML>table.rows[j].cells[0].innerHTML)
-					k=j;
-			}
-			if (k>i)
-			{
-				tmp=table.rows[i].cells[0].innerHTML;
-				table.rows[i].cells[0].innerHTML=table.rows[k].cells[0].innerHTML;
-				table.rows[k].cells[0].innerHTML=tmp;
-			}
-		}
-	}
-	
-	//调用排序
-	CellSort('table2');
-
-	function mergeCell(table, startRow, endRow, col) {  
+	function mergeTable1Cell(table, startRow, endRow, col) {  
         var tb = document.getElementById(table);  
-        if (col >= tb.rows[0].cells.length) {  
+        if (col >= tb.rows[0].cells.length-1) {  
             return;  
         }  
         if (col == 0) { endRow = tb.rows.length-1; }  
         for (var i = startRow; i < endRow; i++) {
 
             if (tb.rows[startRow].cells[col].innerHTML == tb.rows[i + 1].cells[col].innerHTML) {  
-            	tb.rows[i + 1].cells[col].style.display='none';
-            	tb.rows[i + 1].cells[7].style.display='none';
+            	if( i!=6)
+            		tb.rows[i + 1].cells[col].style.display='none';
                 tb.rows[startRow].cells[col].rowSpan = (tb.rows[startRow].cells[col].rowSpan | 0) + 1;
-                
-                if( table=="table1" )
-                {
-                	if(col ==1)
-                	{
-                		tb.rows[startRow].cells[7].rowSpan = tb.rows[startRow].cells[7].rowSpan + 1;
-                		tb.rows[i].cells[7].innerHTML = tb.rows[startRow].cells[7].rowSpan;
-                	}
+
+            	if(col ==1)
+            	{
+            		tb.rows[i + 1].cells[7].style.display='none';
+            		tb.rows[startRow].cells[7].rowSpan = tb.rows[startRow].cells[7].rowSpan + 1;
+            		tb.rows[i].cells[7].innerHTML = tb.rows[startRow].cells[1].rowSpan;
+            	}
                 	
-                }
-                else if( table=="table2" )
-                {
-                	if(col==0)
-                	{
-                		tb.rows[startRow].cells[7].rowSpan = tb.rows[startRow].cells[7].rowSpan + 1;
-                		tb.rows[i].cells[7].innerHTML = tb.rows[startRow].cells[7].rowSpan;
-                	}
-                }
+           
                 if (i == endRow - 1 && startRow != endRow) {  
-                	mergeCell(table, startRow, endRow, col + 1);  
+                	mergeTable1Cell(table, startRow, endRow, col + 1);  
                 }  
-            } else {
-            	mergeCell(table, startRow, i + 0, col + 1);  
+            }
+            else {
+                	
+            		if(tb.rows[startRow].cells[1].rowSpan==1)
+            		{
+            			tb.rows[i+1].cells[7].innerHTML = 1;
+            		}
+
+            		mergeTable1Cell(table, startRow, i + 0, col + 1);  
+	                startRow = i + 1;  
+            }
+        }
+    }
+	
+	function mergeTable2Cell(table, startRow, endRow, col) {  
+        var tb = document.getElementById(table);  
+        if (col >= tb.rows[0].cells.length-1) {  
+            return;  
+        }  
+        if (col == 0) { endRow = tb.rows.length-1; }  
+        for (var i = startRow; i < endRow; i++) {
+
+            if (tb.rows[startRow].cells[0].innerHTML == tb.rows[i + 1].cells[0].innerHTML) {  
+
+            	tb.rows[i + 1].cells[0].style.display='none';
+                tb.rows[startRow].cells[0].rowSpan = (tb.rows[startRow].cells[0].rowSpan | 0) + 1;
+                
+
+                tb.rows[i + 1].cells[7].style.display='none';
+                tb.rows[startRow].cells[7].rowSpan = tb.rows[startRow].cells[7].rowSpan + 1;
+                tb.rows[startRow].cells[7].innerHTML = tb.rows[startRow].cells[0].rowSpan;
+
+            }
+            else
+            {
+            	if(tb.rows[startRow].cells[0].rowSpan==1)
+        		{
+            		tb.rows[startRow].cells[7].innerHTML = 1;
+        		}
                 startRow = i + 1;  
             }
         }
     }
 	
-	mergeCell('table1',1,0,0)
-	mergeCell('table2',1,0,0)
-	
+	mergeTable1Cell('table1',1,0,0)
+	mergeTable2Cell('table2',1,0,0)
+
 	//radio相关
 	$("input[name='mode']").each(function(){
 	    if($(this).attr("checked"))
